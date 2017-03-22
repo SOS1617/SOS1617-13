e1a1f97fccab5b6bb493183a99c31b6b0874db72
sos161713ldvc <lucdelcan@alum.us.es>
2017-03-22 13:55:29 +0000

    chabges

--- index.1.js
+++ index.1.js
@@ -25,7 +25,9 @@ MongoClient.content(mdbURL, {native_parser:true},function (err,database){
     
     app.listen(port, ()=> {
   console.log("Magic is happening on port " + port);  
+    
 
+});
 });
 
 var app = express();
@@ -388,5 +390,5 @@ app.delete(BASE_API_PATH + "/goals/:city", function (request, response) {
     }
 });
 
-});
+
 
